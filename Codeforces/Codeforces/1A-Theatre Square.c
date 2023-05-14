d40f5466836e1ca199d2707075bcc6c1ea856f65
MD.TAMIM AHMED FAHIM <130177153+t-amim@users.noreply.github.com>
2023-05-14 15:43:25 +0600

    Add files via upload

---
+++ Codeforces/Codeforces/1A-Theatre Square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+    long long int n,m,a,x,y,product;
+    scanf("%lld%lld%lld",&n,&m,&a);
+    if(n%a==0){x=n/a;}
+    else{x=(n/a)+1;}
+    if(m%a==0){y=m/a;}
+    else{y=(m/a)+1;}
+
+    printf("%lld",x*y);
+return 0;
+}
---
+++ Codeforces/Codeforces/50A-Domino piling.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=(a*b)*.5;
+    printf("%d",c);
+return 0;
+}
